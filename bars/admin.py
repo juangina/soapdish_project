391d904d3daf6a6526b258059d00f4044edc0955
Juan Johnson <ericrenee21@gmail.com>
2021-12-28 21:55:51 -0500

    Revised Admin Panel with additional editing features

--- bars/admin.py
+++ bars/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Bar
 
 class BarsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'recipe','price', 'batch_code','is_cured')
+    list_display = ('id', 'name', 'recipe','nutrients','exfolients','price', 'batch_code','is_cured')
     list_display_links = ('id', 'name')
-    list_editable = ('is_cured',)
+    list_editable = ('nutrients','exfolients','is_cured',)
     search_fields = ('name',)
     list_per_page = 50
 
