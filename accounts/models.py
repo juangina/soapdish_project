03830b922906223a63fe238bce3d523eb21189d1
Juan Johnson <ericrenee21@gmail.com>
2022-01-10 10:06:17 -0500

    Added more migrations to account app

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from store.models import Customer
+from bars.models import Bar
 
 class PrimaryAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
@@ -23,16 +24,14 @@ class Profile(models.Model):
     location = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField(
-        null=True, blank=True,upload_to='photos/%Y/%m/%d/', default='/placeholder.png')
+    profile_image = models.ImageField(null=True, blank=True,upload_to='photos/%Y/%m/%d/', default='/placeholder.png')
     social_github = models.CharField(max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
     social_linkedin = models.CharField(max_length=200, blank=True, null=True)
     social_youtube = models.CharField(max_length=200, blank=True, null=True)
     social_website = models.CharField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.username)
@@ -49,33 +48,51 @@ class Profile(models.Model):
         return url
 
 class Skill(models.Model):
-    owner = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.name)
 
 class Message(models.Model):
-    sender = models.ForeignKey(
-        Profile, on_delete=models.SET_NULL, null=True, blank=True)
-    recipient = models.ForeignKey(
-        Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="messages")
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="messages")
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
     subject = models.CharField(max_length=200, null=True, blank=True)
     body = models.TextField()
     is_read = models.BooleanField(default=False, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
 
     def __str__(self):
         return self.subject
 
     class Meta:
         ordering = ['is_read', '-created']
+
+class Review(models.Model):
+    VOTE_TYPE = (('up', 'Up Vote'),('down', 'Down Vote'),)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Bar, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['owner', 'project']]
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
